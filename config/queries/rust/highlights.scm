75cc5e49506d99b8d19209216f2d2a74eb27bb57
Yves Ineichen <iff@yvesineichen.com>
2025-09-15 21:16:43 +0200

    fix rust highlights after installing all grammars

--- config/queries/rust/highlights.scm
+++ config/queries/rust/highlights.scm
@@ -1,22 +1,18 @@
 ;; extends
 
-;; also want to do something for doc_comment
-;; TODO also how to set style/colors?
-;; vim.api.nvim_set_hl(0, "@comment.documentation", { fg = "#7c7c7c" })
-
 (
   (doc_comment) @comment.documentation
   (#set! priority 126)
 )
 
-(
-  (line_comment) @comment.todo
-  (#match? @comment.todo "TODO")
-  (#set! priority 126)
-)
-
-(
-  (line_comment) @comment.note
-  (#match? @comment.note "NOTE")
-  (#set! priority 126)
-)
+; (
+;   (line_comment) @comment.todo
+;   (#match? @comment.todo "TODO")
+;   (#set! priority 126)
+; )
+;
+; (
+;   (line_comment) @comment.note
+;   (#match? @comment.note "NOTE")
+;   (#set! priority 126)
+; )
