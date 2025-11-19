30fbf185f70a7809e0dfe7b532957f4b24b7dd46
Yves Ineichen <iff@yvesineichen.com>
2025-11-19 16:14:02 +0100

    lsps::nix: options

--- config/lua/yi/lsps/nix.lua
+++ config/lua/yi/lsps/nix.lua
@@ -2,10 +2,21 @@ local M = {}
 
 function M.setup(capabilities)
     vim.lsp.config("nil_ls", {
-        cmd = { "nil" },
         capabilities = capabilities,
-        root_markers = { "flake.nix", ".git" },
-        filetypes = { "nix" },
+        settings = { -- https://github.com/oxalica/nil/blob/main/docs/configuration.md
+
+            ["nil"] = {
+                nix = {
+                    maxMemoryMB = 10000,
+                    flake = {
+                        autoArchive = true,
+                        -- TODO this could be dangerous?
+                        autoEvalInputs = true,
+                        nixpkgsInputName = "nixpkgs",
+                    },
+                },
+            },
+        },
     })
 
     vim.lsp.enable("nil_ls")
