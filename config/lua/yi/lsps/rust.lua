3c767ac0f0508c318178eddc9f4c01063d1cc626
Yves Ineichen <iff@yvesineichen.com>
2025-11-24 08:57:11 +0100

    rust: saner config and using clippy

--- config/lua/yi/lsps/rust.lua
+++ config/lua/yi/lsps/rust.lua
@@ -36,64 +36,83 @@ function M.setup_(capabilities)
 end
 
 function M.setup(capabilities)
-    vim.lsp.config(
-        "rust_analyzer",
-        --- @type vim.lsp.ClientConfig
-        {
-            cmd = { "rust-analyzer" },
-            filetypes = { "rust" },
-            capabilities = vim.tbl_deep_extend("force", capabilities, {
-                experimental = {
-                    commands = {
-                        commands = {
-                            "rust-analyzer.showReferences",
-                        },
-                    },
+    -- vim.lsp.config(
+    --     "rust_analyzer",
+    --     --- @type vim.lsp.ClientConfig
+    --     {
+    --         cmd = { "rust-analyzer" },
+    --         filetypes = { "rust" },
+    --         capabilities = vim.tbl_deep_extend("force", capabilities, {
+    --             experimental = {
+    --                 commands = {
+    --                     commands = {
+    --                         "rust-analyzer.showReferences",
+    --                     },
+    --                 },
+    --             },
+    --         }),
+    --         settings = {
+    --             ["rust-analyzer"] = {
+    --                 checkOnSave = false,
+    --                 -- procMacro = {
+    --                 --     enable = false,
+    --                 -- },
+    --                 -- diagnostics = {
+    --                 --     enable = true,
+    --                 --     disabled = {},
+    --                 --     enableExperimental = false,
+    --                 -- },
+    --                 -- hover = {
+    --                 --     actions = {
+    --                 --         enable = false,
+    --                 --     },
+    --                 -- },
+    --                 lens = {
+    --                     enable = true,
+    --                     run = {
+    --                         enable = true,
+    --                     },
+    --                     implementations = {
+    --                         enable = true,
+    --                     },
+    --                     references = {
+    --                         adt = {
+    --                             enable = true,
+    --                         },
+    --                         method = {
+    --                             enable = true,
+    --                         },
+    --                         trait = {
+    --                             enable = true,
+    --                         },
+    --                         enumVariant = {
+    --                             enable = true,
+    --                         },
+    --                     },
+    --                 },
+    --             },
+    --         },
+    --     }
+    -- )
+
+    vim.lsp.config("rust_analyzer", {
+        cmd = { "rust-analyzer" },
+        filetypes = { "rust" },
+        capabilities = capabilities,
+        settings = {
+            ["rust-analyzer"] = {
+                check = {
+                    command = "clippy",
                 },
-            }),
-            settings = {
-                ["rust-analyzer"] = {
-                    checkOnSave = false,
-                    -- procMacro = {
-                    --     enable = false,
-                    -- },
-                    -- diagnostics = {
-                    --     enable = true,
-                    --     disabled = {},
-                    --     enableExperimental = false,
-                    -- },
-                    -- hover = {
-                    --     actions = {
-                    --         enable = false,
-                    --     },
-                    -- },
-                    lens = {
+                diagnostics = {
+                    enable = true,
+                    experimental = {
                         enable = true,
-                        run = {
-                            enable = true,
-                        },
-                        implementations = {
-                            enable = true,
-                        },
-                        references = {
-                            adt = {
-                                enable = true,
-                            },
-                            method = {
-                                enable = true,
-                            },
-                            trait = {
-                                enable = true,
-                            },
-                            enumVariant = {
-                                enable = true,
-                            },
-                        },
                     },
                 },
             },
-        }
-    )
+        },
+    })
 
     vim.lsp.enable("rust_analyzer")
 end
