86b39eeca47dfb616ff234b6ee37d41c83528d79
Yves Ineichen <iff@yvesineichen.com>
2026-01-31 10:06:57 +0100

    lsps: typscript with biome and less config

--- config/lua/yi/lsps/typescript.lua
+++ config/lua/yi/lsps/typescript.lua
@@ -3,8 +3,36 @@ local M = {}
 function M.setup(capabilities)
     vim.lsp.config("ts_ls", {
         capabilities = capabilities,
+    --     cmd = { "typescript-language-server", "--stdio" },
+    --     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+    --     root_dir = vim.fs.root(0, { "package.json", "tsconfig.json", "jsconfig.json", ".git" }),
+    --     settings = {
+    --         typescript = {
+    --             inlayHints = {
+    --                 includeInlayParameterNameHints = "all",
+    --                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+    --                 includeInlayFunctionParameterTypeHints = true,
+    --                 includeInlayVariableTypeHints = true,
+    --                 includeInlayPropertyDeclarationTypeHints = true,
+    --                 includeInlayFunctionLikeReturnTypeHints = true,
+    --                 includeInlayEnumMemberValueHints = true,
+    --             },
+    --         },
+    --         javascript = {
+    --             inlayHints = {
+    --                 includeInlayParameterNameHints = "all",
+    --                 includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+    --                 includeInlayFunctionParameterTypeHints = true,
+    --                 includeInlayVariableTypeHints = true,
+    --                 includeInlayPropertyDeclarationTypeHints = true,
+    --                 includeInlayFunctionLikeReturnTypeHints = true,
+    --                 includeInlayEnumMemberValueHints = true,
+    --             },
+    --         },
+    --     },
     })
 
+    vim.lsp.enable("biome")
     vim.lsp.enable("ts_ls")
 end
 
