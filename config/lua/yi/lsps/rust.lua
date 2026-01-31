9a542f588e933b238debca1e9a2c4b6ee6464327
Yves Ineichen <iff@yvesineichen.com>
2026-01-31 10:15:03 +0100

    lsps: rust cleanup and comments

--- config/lua/yi/lsps/rust.lua
+++ config/lua/yi/lsps/rust.lua
@@ -1,7 +1,9 @@
 local M = {}
 
-function M.setup_(capabilities)
-    function on_attach(client, bufnr)
+function M.setup_rustaceanvim(capabilities)
+    -- TODO this never worked on osx: spawned two rust-analyzers hogging the cpu (and draining battery)
+    -- for now just switched to the buildin rust lsp
+    local function on_attach(_, bufnr)
         local function nmap(lhs, rhs, desc)
             vim.keymap.set("n", lhs, rhs, { buffer = bufnr, desc = desc })
         end
@@ -14,93 +16,45 @@ function M.setup_(capabilities)
             on_attach = on_attach,
             capabilities = capabilities,
         },
-        default_settings = {
-            ["rust-analyzer"] = {
-                checkOnSave = false,
-                procMacro = {
-                    enable = false,
-                },
-                diagnostics = {
-                    enable = true,
-                    disabled = {},
-                    enableExperimental = false,
-                },
-                hover = {
-                    actions = {
-                        enable = false,
-                    },
-                },
-            },
-        },
+        --     default_settings = {
+        --         ["rust-analyzer"] = {
+        --             checkOnSave = false,
+        --             procMacro = {
+        --                 enable = false,
+        --             },
+        --             diagnostics = {
+        --                 enable = true,
+        --                 disabled = {},
+        --                 enableExperimental = false,
+        --             },
+        --             hover = {
+        --                 actions = {
+        --                     enable = false,
+        --                 },
+        --             },
+        --         },
+        --     },
     }
 end
 
 function M.setup(capabilities)
-    -- vim.lsp.config(
-    --     "rust_analyzer",
-    --     --- @type vim.lsp.ClientConfig
-    --     {
-    --         cmd = { "rust-analyzer" },
-    --         filetypes = { "rust" },
-    --         capabilities = vim.tbl_deep_extend("force", capabilities, {
-    --             experimental = {
-    --                 commands = {
-    --                     commands = {
-    --                         "rust-analyzer.showReferences",
-    --                     },
-    --                 },
-    --             },
-    --         }),
-    --         settings = {
-    --             ["rust-analyzer"] = {
-    --                 checkOnSave = false,
-    --                 -- procMacro = {
-    --                 --     enable = false,
-    --                 -- },
-    --                 -- diagnostics = {
-    --                 --     enable = true,
-    --                 --     disabled = {},
-    --                 --     enableExperimental = false,
-    --                 -- },
-    --                 -- hover = {
-    --                 --     actions = {
-    --                 --         enable = false,
-    --                 --     },
-    --                 -- },
-    --                 lens = {
-    --                     enable = true,
-    --                     run = {
-    --                         enable = true,
-    --                     },
-    --                     implementations = {
-    --                         enable = true,
-    --                     },
-    --                     references = {
-    --                         adt = {
-    --                             enable = true,
-    --                         },
-    --                         method = {
-    --                             enable = true,
-    --                         },
-    --                         trait = {
-    --                             enable = true,
-    --                         },
-    --                         enumVariant = {
-    --                             enable = true,
-    --                         },
-    --                     },
-    --                 },
-    --             },
-    --         },
-    --     }
-    -- )
-
+    -- --- @type vim.lsp.ClientConfig
     vim.lsp.config("rust_analyzer", {
         cmd = { "rust-analyzer" },
         filetypes = { "rust" },
         capabilities = capabilities,
+        -- capabilities = vim.tbl_deep_extend("force", capabilities, {
+        --     experimental = {
+        --         commands = {
+        --             commands = {
+        --                 "rust-analyzer.showReferences",
+        --             },
+        --         },
+        --     },
+        -- }),
         settings = {
             ["rust-analyzer"] = {
+                -- checkOnSave = false,
                 cargo = {
                     allFeatures = true,
                 },
@@ -113,6 +67,32 @@ function M.setup(capabilities)
                         enable = true,
                     },
                 },
+                -- procMacro = {
+                --     enable = false,
+                -- },
+                -- lens = {
+                --     enable = true,
+                --     run = {
+                --         enable = true,
+                --     },
+                --     implementations = {
+                --         enable = true,
+                --     },
+                --     references = {
+                --         adt = {
+                --             enable = true,
+                --         },
+                --         method = {
+                --             enable = true,
+                --         },
+                --         trait = {
+                --             enable = true,
+                --         },
+                --         enumVariant = {
+                --             enable = true,
+                --         },
+                --     },
+                -- },
             },
         },
     })
