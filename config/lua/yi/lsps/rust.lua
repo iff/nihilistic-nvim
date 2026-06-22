0c51301097d36d50010051cbd9cbd1acde01ffb5
Yves Ineichen <iff@yvesineichen.com>
2026-06-22 08:53:13 +0200

    rust: lsp config for rustc

--- config/lua/yi/lsps/rust.lua
+++ config/lua/yi/lsps/rust.lua
@@ -1,48 +1,38 @@
 local M = {}
 
-function M.setup_rustaceanvim(capabilities)
-    -- TODO this never worked on osx: spawned two rust-analyzers hogging the cpu (and draining battery)
-    -- for now just switched to the buildin rust lsp
-    local function on_attach(_, bufnr)
-        local function nmap(lhs, rhs, desc)
-            vim.keymap.set("n", lhs, rhs, { buffer = bufnr, desc = desc })
-        end
-
-        nmap("ad", ":RustLsp openDocs<CR>", "go to docs")
-    end
-
-    vim.g.rustaceanvim = {
-        server = {
-            on_attach = on_attach,
-            capabilities = capabilities,
-        },
-        --     default_settings = {
-        --         ["rust-analyzer"] = {
-        --             checkOnSave = false,
-        --             procMacro = {
-        --                 enable = false,
-        --             },
-        --             diagnostics = {
-        --                 enable = true,
-        --                 disabled = {},
-        --                 enableExperimental = false,
-        --             },
-        --             hover = {
-        --                 actions = {
-        --                     enable = false,
-        --                 },
-        --             },
-        --         },
-        --     },
-    }
-end
-
 function M.setup(capabilities)
+    local default_root_dir = vim.lsp.config["rust_analyzer"].root_dir
+    local default_before_init = vim.lsp.config["rust_analyzer"].before_init
+
     -- --- @type vim.lsp.ClientConfig
     vim.lsp.config("rust_analyzer", {
         cmd = { "rust-analyzer" },
         filetypes = { "rust" },
         capabilities = capabilities,
+        root_dir = function(bufnr, on_dir)
+            local git_root = vim.fs.root(bufnr, { ".git" })
+            if git_root and vim.uv.fs_stat(vim.fs.joinpath(git_root, "src/etc/rust_analyzer_zed.json")) then
+                on_dir(git_root)
+                return
+            end
+            default_root_dir(bufnr, on_dir)
+        end,
+        before_init = function(init_params, config)
+            local settings_path = vim.fs.joinpath(config.root_dir, "src/etc/rust_analyzer_zed.json")
+            if vim.uv.fs_stat(settings_path) then
+                local file = io.open(settings_path)
+                local json = vim.json.decode(file:read("*a"), { skip_comments = true })
+                file:close()
+                config.settings["rust-analyzer"] = vim.tbl_deep_extend(
+                    "force",
+                    config.settings["rust-analyzer"] or {},
+                    json.lsp["rust-analyzer"].initialization_options
+                )
+            end
+            if default_before_init then
+                default_before_init(init_params, config)
+            end
+        end,
         -- capabilities = vim.tbl_deep_extend("force", capabilities, {
         --     experimental = {
         --         commands = {
