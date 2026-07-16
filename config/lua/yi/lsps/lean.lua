0acee8e7c11fb2b2366d7de50b6b1618f28349b8
Yves Ineichen <iff@yvesineichen.com>
2026-07-16 13:46:53 +0200

    lean: fix lsp setup

--- config/lua/yi/lsps/lean.lua
+++ config/lua/yi/lsps/lean.lua
@@ -1,6 +1,12 @@
 local M = {}
 
 function M.setup()
+    vim.lsp.config("leanls", {
+        cmd = { "lake", "serve", "--" },
+        filetypes = { "lean" },
+        root_markers = { "lakefile.toml", "lakefile.lean", "lean-toolchain", ".git" },
+    })
+
     vim.lsp.enable("leanls")
 end
 
