61e505ead677437dfcb9410c691040c6b197d7a4
Yves Ineichen <iff@yvesineichen.com>
2026-09-09 07:59:22 +0200

    disable modes in flash and underline match

--- config/lua/yi/flash.lua
+++ config/lua/yi/flash.lua
@@ -4,7 +4,17 @@ function mod.setup()
     require("flash").setup {
         -- based on https://colemakmods.github.io/mod-dh/model.html
         labels = "ntseriufhdywoa",
+        modes = {
+            char = {
+                -- NOTE this maps any of FfTt, and mappings.lua has a problem with that when deleting all default mappings
+                -- it will break our submodes
+                enabled = false,
+            },
+        },
     }
+    vim.api.nvim_set_hl(0, "FlashCurrent", { underline = true })
+    vim.api.nvim_set_hl(0, "FlashMatch", { underline = true })
+    vim.api.nvim_set_hl(0, "FlashLabel", { link = "Search" })
 end
 
 return mod
